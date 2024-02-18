a2ff876d8cdd5c667bcb63c6b025ddc057d98ddd
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-02-18 20:52:20 +0100

    states for bookingpage

--- src/components/ChoseNumberOfPersons.tsx
+++ src/components/ChoseNumberOfPersons.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+
 export const ChoseNumberOfPersons = () => {
+  const [nrOfGuests, setNrOfGuests] = useState(0);
+
   const buttons = [];
   for (let i = 1; i <= 90; i++) {
-    buttons.push(<button key={i}>{i}</button>);
+    buttons.push(
+      <button key={i} onClick={() => setNrOfGuests(i)}>
+        {i}
+      </button>
+    );
   }
 
   return (
     <div className="chosenumberofpersons-container">
       <h2>Välj antal gäster</h2>
-
+      <p>Valt antal: {nrOfGuests}</p>
       <div className="buttons-container">{buttons}</div>
     </div>
   );
