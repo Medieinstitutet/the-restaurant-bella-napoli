96240512ce600c5430e0f749cb41649d05a683f1
Carl Cleverborn <carl.cleverborn@hotmail.se>
2024-02-20 18:15:34 +0100

    animations

--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -2,8 +2,18 @@ import { NavLink } from "react-router-dom";
 //import { FoodMenu } from "../components/FoodMenu";
 import "../styles/_Landingpage.scss";
 import Footer from "../components/footer";
+import { useState } from "react";
 
 export const Landingpage = () => {
+  const [activeSection, setActiveSection] = useState<"menu" | "footer" | "">(
+    ""
+  );
+
+  const toggleAnimation = (section: "menu" | "footer") => {
+    setActiveSection(section);
+    setTimeout(() => setActiveSection(""), 1500);
+  };
+
   return (
     <div className="landingpage-background">
       <header>
@@ -12,17 +22,24 @@ export const Landingpage = () => {
             <NavLink to={"/bookingForm"}>Boka bord</NavLink>
           </li>
           <li>
-            <a href="#menu-container">Meny</a>
+            <a href="#menu-container" onClick={() => toggleAnimation("menu")}>
+              Meny
+            </a>
           </li>
           <li>
-            <a href="#kontakt">Kontakt</a>
+            <a href="#kontakt" onClick={() => toggleAnimation("footer")}>
+              Kontakt
+            </a>
           </li>
         </ul>
       </header>
       <div className="welcome-container">
         <h1>Välkommen till Bella Napoli!</h1>
       </div>
-      <div id="menu-container">
+      <div
+        id="menu-container"
+        className={activeSection === "menu" ? "animation-active" : ""}
+      >
         <div className="menutext-container">
           <h2>Meny</h2>
           <p>Risotto</p>
@@ -34,10 +51,13 @@ export const Landingpage = () => {
         </div>
         {/* <FoodMenu /> */}
       </div>
-      <div id="contact-container">
+      <div
+        id="contact-container"
+        className={activeSection === "footer" ? "animation-active" : ""}
+      >
         <Footer />
       </div>
-      <div id="kontakt"></div>
+      {/* <div id="kontakt"></div> */}
     </div>
   );
 };
