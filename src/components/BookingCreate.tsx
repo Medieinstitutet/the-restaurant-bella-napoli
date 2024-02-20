4f0244dcff866143bb78ab00fe1881f7209afacd
Yonatan Zewude <yonatanzewude@hotmail.com>
2024-02-20 10:11:10 +0100

    created Footer component

--- src/components/BookingCreate.tsx
+++ src/components/BookingCreate.tsx
@@ -15,7 +15,7 @@ export const BookingForm = () => {
   const [phone, setPhone] = useState("");
   const [showBookingForm, setShowBookingForm] = useState(false);
 
-  const TotalTables = 15; aas
+  const TotalTables = 15; 
   const restaurantId = import.meta.env.VITE_REST_ID;
 
   useEffect(() => {
